c032bcc1f31b80d4124c58cf7652a3af46d435b8
Paula Galindo <118644708+Gali1234567@users.noreply.github.com>
2023-05-18 15:09:33 -0500

    Create CompararCadenas.java

---
+++ Java/CompararCadenas.java
@@ -0,0 +1,14 @@
+CompararCadenas.java
+class CompararCadenas {
+  public static void main(String[] args){
+  String cadena1 ="Los Amigos";
+  String cadena2 ="los amigos";
+  //Se instancia objeto del tipo string
+  Object objCadena = cadena1;
+  //Determinar si las cadenas son iguales
+  System.out.println(cadena1.compareTo(cadena2));
+  System.out.println(cadena1.compareToIgnoreCase(cadena2));
+  System.out.println (cadena1.equals(cadena2));
+  System.out.println(cadena1=cadena2);
+}
+}
